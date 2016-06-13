b961c785f11e10bba03af9e4f3849ff20ef7d42e
Lars Eckle <Lars.Eckle@posteo.net>
2016-06-14 00:28:45 +0800

    final

--- run_analysis.R
+++ run_analysis.R
@@ -20,7 +20,7 @@ if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl,destfile="./data/Dataset.zip",method="auto")
 
-#unpack
+# unpack
 unzip(zipfile="./data/Dataset.zip",exdir="./data")
 mydatadirectory <- file.path("./data" , "UCI HAR Dataset")
 
